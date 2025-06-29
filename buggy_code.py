6c6d20b62ba4d81ff5b76e247b2227514958801a
ofri036 <555ofri@gmail.com>
2025-06-29 17:23:00 +0300

    after redo

--- buggy_code.py
+++ buggy_code.py
@@ -1,40 +1,19 @@
-list1 = [2, 4, 6, 8]
-list2 = [10, 20, 30, 40]
-the_list = list1 + list2
-
-def sum_list(list):
-    '''
-    this function gets a list and sums it
-    :parm list
-    :type list
-    :return: list
-    :rtype: list
-    '''
-    sum = 0
-    for number in the_list:
-        sum += number
-    return sum
-
-def element_count(list) :
-    '''
-    this function gets a list and counts elements
-    :parm list
-    :type list
-    :return: number of elements
-    :rtype: int
-    '''
-    amount = 0
-    for element in the_list:
-        amount += 1
-    return amount
-
 def average_list():
-    ''''
-    this function gets the sum of the list,and the number of its elements, and averages it.
+    '''
+    this function Gets two lists, concatenates them, and calculates the average of the total list
     :return: None
     '''
-    the_average = sum_list(the_list) / element_count(the_list)
-    print(f"the average is {the_average}")
+    list1 = [2, 4, 6, 8]
+    list2 = [10, 20, 30, 40]
+    the_list = list1 + list2
+    sum = 0
+    amount = 0
+    for number in the_list:
+        sum += number
+        amount += 1
+    average = sum / amount
+    print(f"the average is {average}")
+
 
 def main():
     average_list()
